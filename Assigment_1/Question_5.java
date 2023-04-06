430985fd7aafd386c8b7f6c445b4c04a3b9587b2
Ayush Kachhap <120742004+Insane30@users.noreply.github.com>
2023-04-06 18:53:11 +0530

    Update Question_5.java

--- Assigment_1/Question_5.java
+++ Assigment_1/Question_5.java
@@ -3,14 +3,20 @@ import java.util.*;
 public class Question_5 {
     public static int sum_Of_Digits(int n) {
         int sum=0;
-        while(sum>=9){
-
+        while(n!=0){
+         sum+=n%10;
+            n=n/10;
         }
+        if(sum>9){
+            sum=sum_Of_Digits(sum)
+            }
 return sum;
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
+        System.out.print("Enter a number :");
+            int num=sc.nextInt();
+        System.out.println("Sum of digits of "+num+""+"until the number is the single digit"+sum_Of_Digits(num));
     }
 
 
